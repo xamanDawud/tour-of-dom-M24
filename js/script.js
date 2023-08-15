9d65d2868aaf5593e86a25f3bf5d97d753173921
xamanDawud <ahmeddaudking100@gmail.com>
2023-08-15 15:29:49 +0600

    some dom program has been explained

--- js/script.js
+++ js/script.js
@@ -18,3 +18,52 @@ let placeName = document.getElementsByClassName("place-name");
 for (let place of placeName) {
   //   console.log(place.innerHTML);
 }
+
+let mainSectionParent = document.getElementById("body");
+
+let newSection = document.createElement("section");
+let newHeadline = document.createElement("h1");
+let newUl = document.createElement("ul");
+let newLi = document.createElement("li");
+let newLi2 = document.createElement("li");
+let newLi3 = document.createElement("li");
+let newLi4 = document.createElement("li");
+
+newHeadline.innerText = "Everyday working list";
+newSection.appendChild(newHeadline);
+
+newSection.appendChild(newUl);
+newLi.innerText = "Wake up before 7am";
+newLi2.innerText = "Read 5 page of any book daily";
+newLi3.innerText = "Memorize 5 words daily";
+newLi4.innerText = "Belive yourself and take risk";
+newUl.appendChild(newLi);
+newUl.appendChild(newLi2);
+newUl.appendChild(newLi3);
+newUl.appendChild(newLi4);
+
+mainSectionParent.appendChild(newSection);
+
+let brandNewSection = document.createElement("section");
+brandNewSection.innerHTML = `
+  <h1>Beset Five Books Name</h1>
+  <ul> 
+    <li>Pride and Prejudice</li>
+    <li>It Starts with Us</li>
+    <li>It Ends with Us</li>
+    <li>The sun and her flowers</li>
+    <li>Milk and Honey</li>
+  </ul>
+`;
+brandNewSection.classList.add("brandNewSection");
+mainSectionParent.appendChild(brandNewSection);
+
+let allSection = document.getElementsByTagName("section");
+// console.log(allSection);
+for (let section of allSection) {
+  let indiSection = section;
+  indiSection.style.backgroundColor = "gray";
+  indiSection.style.padding = "10px";
+  indiSection.style.margin = "10px";
+  indiSection.style.borderRadius = "15px";
+}
